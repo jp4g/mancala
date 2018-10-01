023bf4c89f07b963f9f27737471c70ec71fff903
Peter Cooke <petercooke536@gmail.com>
2018-10-01 10:48:58 -0400

    UI Update

--- Mancala/src/GameInteraction.java
+++ Mancala/src/GameInteraction.java
@@ -1,42 +1,46 @@
-import java.awt.Dimension;
+import java.awt.BorderLayout;
 import java.awt.Graphics;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URL;
 
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class GameInteraction extends JPanel {
 
-	public JPanel panel;
+	public static boolean hasGameStarted;
+	
+	public static JPanel panel;
 	public JFrame window = new JFrame("Mancala");
 	
 	public Toolkit tk = Toolkit.getDefaultToolkit();
-	public BufferedImage mancalaBoard = null;
-	public BufferedImage mancala = null;
-	public BufferedImage winImage = null;
-	public BufferedImage loseImage = null;
-	public BufferedImage drawImage = null;
+	
+	//===================================================================== Menu Buttons
+	public static JButton startButton;
+	public static JButton quitButton;
+
+	//===================================================================== Image Resources
+	public static BufferedImage mancalaBoard = null;
+	public static BufferedImage mancala = null;
+	public static BufferedImage winImage = null;
+	public static BufferedImage loseImage = null;
+	public static BufferedImage drawImage = null;
 
 
 	public GameInteraction() {
 
-		mancalaBoard = loadImage("MancalaBoard.png");
-		mancala = loadImage("Mancala.png");
-		
-		
-		//drawImage = loadImage("Draw.png");
-		//winImage = loadImage("Winner.png");
-		//loseImage = loadImage("Loser.png");
+		hasGameStarted = false;
+		loadImages();
 
 		window.setBounds(tk.getScreenSize().width / 2 - 600, tk.getScreenSize().height / 2 - 400, 1200, 800);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -71,36 +75,63 @@ public class GameInteraction extends JPanel {
 		});
 		
 		panel = new JPanel();
-		panel.setLayout(null);
-		window.add(panel);
+		panel.setLayout(new BorderLayout());
 		
-		JButton startButton= new JButton("Start");
-		startButton.setBounds(500, 500, 180, 60);
+		
+		startButton = new JButton("Start");
+		quitButton = new JButton("Quit");
+		
+		startButton.setBounds(500, 600, 180, 60);
+		startButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				hasGameStarted = true;
+				repaint();
+				startGame();
+			}
+		});
 		panel.add(startButton);
 		
-		JButton quitButton= new JButton("Quit");
-		quitButton.setBounds(500, 600, 180, 60);
-		panel.add(quitButton);
-		
+		quitButton.setBounds(500, 670, 180, 60);
 		quitButton.addActionListener(new ActionListener() {	
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				window.dispose();
 			}
 		});
+		panel.add(quitButton);
 		
+		JLabel m  = new JLabel(new ImageIcon(mancala));
+		panel.add(m);
 		
+		window.add(panel);
 	}
 
 	@Override
 	protected void paintComponent(Graphics g) {
-		//super.paintComponent(g);
-		g.drawImage(mancala, 90, 50, null);
+		super.paintComponent(g);
+		if(!hasGameStarted) {
+			g.drawImage(mancala, 90, 50, null);
+		} else {
+			startButton.setVisible(false);
+			quitButton.setVisible(false);
+			g.drawImage(mancalaBoard, 0, 0, null);
+		}
+		
 		
 		//Every board object will have to have a defined space (eg rectangle) in their class
 		//It will also need a draw() method to draw the amount of stones it has
 		//I will then loop through every object after each turn and call the paint method
 		//will probably have to put in a sleep so it is not instant
+	}
+	
+	public static void loadImages() {
+		mancalaBoard = loadImage("MancalaBoard.png");
+		mancala = loadImage("Mancala.png");
+		
+		//drawImage = loadImage("Draw.png");
+		//winImage = loadImage("Winner.png");
+		//loseImage = loadImage("Loser.png");
 	}
 
 	public static BufferedImage loadImage(String i) {
@@ -113,6 +144,17 @@ public class GameInteraction extends JPanel {
 			e.printStackTrace();
 		}
 		return null;
+	}
+	
+	/**
+	 * Put code in here for what you want to happen when the game starts
+	 */
+	public static void startGame() {
+		
+	}
+	
+	public static void resetGame() {
+		
 	}
 	
 
